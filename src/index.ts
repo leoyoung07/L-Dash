db415d3c116d4200fc4a0d2037b17a0e364615a1
Leo Young <420760135@qq.com>
2018-07-31 12:34:00 +0800

    feat: add typeOf

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
 export default class LDash {
+
   public static trimStr(str: string) {
     return str.replace(/^\s*|\s*$/gi, '');
   }
+
+  public static typeOf(obj: any) {
+    return Object.prototype.toString.call(obj);
+  }
+
+  public static isString(obj: any) {
+    return LDash.typeOf(obj) === '[object String]';
+  }
+
+  constructor(
+    private target: string
+  ) {
+  }
+
+  public trim() {
+    if (LDash.isString(this.target)) {
+      return LDash.trimStr(this.target);
+    } else {
+      return this.target;
+    }
+  }
+
 }
--- test/index.ts
+++ test/index.ts
@@ -6,3 +6,21 @@ describe('trimStr', () => {
     expect(LDash.trimStr(' hello, world ')).equal('hello, world');
   });
 });
+
+describe('typeOf', () => {
+  it('should return the type of param', () => {
+    expect(LDash.typeOf('hello')).equal('[object String]');
+  });
+});
+
+describe('isString', () => {
+  it('should return is string or not', () => {
+    expect(LDash.isString('hello')).equal(true);
+  });
+});
+
+describe('new LDash(string).trim', () => {
+  it('should trim a string', () => {
+    expect((new LDash(' hello, world ')).trim()).equal('hello, world');
+  });
+});
